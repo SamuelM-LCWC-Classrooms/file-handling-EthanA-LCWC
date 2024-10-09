c644b34bb68bb217e294ff84b7b797c366880ec1
EthanA-LCWC <144792227+EthanA-LCWC@users.noreply.github.com>
2024-10-09 14:36:27 +0000

    I struggled with task 2, but I figured it out eventually

--- task.py
+++ task.py
@@ -3,26 +3,24 @@ def task_1():
     document = open("task_1.txt", "r")
     total = 0
     for number in document:
-        num = document.readline()
-        total += int(num)
+        total += int(number)
     document.close
     return total
 
-def task_2(menu):
+def task_2():
     total = 0
+    loop = "Yes"
+    menu = input("Please enter your item codes: ")
+    menu = menu.split(",")
+    menu = list(menu)
     for item in menu:
         document = open("menu.csv", "r")
-        print(item)
         for line in document:
-            #print("B")
-            #print(line)
             line = line.split(",")
             if item == line[0]:
                 cost = line[2]
                 cost = cost.strip()
-                print(cost)
                 total += float(cost)
-                #print("AAA")
     document.close
     return round(total, 2)
 
@@ -38,6 +36,8 @@ def task_3():
         if text == check_name:
             for key2 in value:
                 new_set.append(value[key2])
+            document.close()
             return (f"Number: {new_set[0]}, Email: {new_set[1]}")
         #print(text)
+    document.close()
     return "User not found."
